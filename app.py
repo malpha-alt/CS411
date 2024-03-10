6e0c5f2cf9d468021e75b0e8c04a55ac501aaeb0
Bruce Casillas <bruce.casillas562@gmail.com>
2024-03-10 00:37:25 -0500

    Added a test to an api call from setlist.fm. Works!

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
-from flask import Flask
+from flask import *
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+import requests
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
 
+
+@app.route('/', methods=['GET', 'POST'])
+def testApi():
+    form = SearchForm()
+    '''if form.validate_on_submit():
+        name = form.ids.data  # Retrieve user input'''
+
+    api_key = "key"
+    headers = {
+        "x-api-key": api_key,
+        "Accept": "application/json"
+    }
+    query_params = {"artistName": "The Beatles"}  # Use the user input here
+    endpoint = "https://api.setlist.fm/rest/1.0/search/artists"
+    response = requests.get(endpoint, params=query_params, headers=headers).json()
+    artistSearch_text = ""
+    iter = 1
+    for item in response.get("artist"):
+        artist_name = item.get("name")
+        artistSearch_text += str(iter) +'. Artist Name: ' + artist_name + '<br>'
+        iter += 1
+    return artistSearch_text
+
+class SearchForm(FlaskForm):
+    ids = StringField("ID",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    csrf_token = HiddenField()
+
+if __name__ == "__main__":
+    # to run me from the command line: <flask --app main run> or <python app.py>
+    #                             
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+"""
 # make sure the database username, database password and
 # database name are correct
 username = 'NotSilent'
@@ -49,8 +89,4 @@ class Accounts(db.Model):
     username = db.Column(db.String)
     password = db.Column(db.String)
     email = db.Column(db.String)
-
-if __name__ == "__main__":
-    # to run me from the command line: <flask --app main run> or <python app.py>
-    #                             
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    """
