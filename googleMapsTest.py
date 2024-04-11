d3e50803a0825a56148e7a3d7189869083fe2507
Bruce Casillas <bruce.casillas562@gmail.com>
2024-04-11 16:20:32 -0400

    Added search functionality and displays results from a setlist api call

--- googleMapsTest.py
+++ googleMapsTest.py
@@ -1,22 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import * # type: ignore
 from wtforms.validators import DataRequired
-import requests
 import os
+import requests
 
 googleMapsTest = Flask(__name__)
 googleMapsTest.config['SECRET_KEY'] = os.urandom(32)
-
 
 @googleMapsTest.route('/', methods=['GET', 'POST'])
 def testApi():
     return render_template('googleMap.html')
 
-@googleMapsTest.route('/search')
+@googleMapsTest.route('/search', methods=['GET', 'POST']) # type: ignore #Search page
 def search():
     form = SearchForm()
-    return render_template('search.html', form=form)
+    search_query = request.form.get('query')
+    results = []
+
+    api_key = "KEY"
+    headers = {
+        "x-api-key": api_key,
+        "Accept": "application/json"
+    }
+    if search_query:
+        query_params = {"artistName": search_query}  # Use the user input here
+        endpoint = "https://api.setlist.fm/rest/1.0/search/artists"
+        response = requests.get(endpoint, params=query_params, headers=headers).json()
+        iter = 1
+        for item in response.get("artist"):
+            artist_name = item.get("name")
+            results.append({'title': artist_name})
+    return render_template('search.html', form=form, results=results)
 
 class SearchForm(FlaskForm):
     ids = StringField("ID",validators=[DataRequired()])
