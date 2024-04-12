de5105cb4cd6cbbadb0af9b5d7211e75c8957778
Bruce Casillas <bruce.casillas562@gmail.com>
2024-04-12 18:41:07 -0400

    Adds ability to save added concerts and send that list to the google map to make markers

--- googleMapsTest.py
+++ googleMapsTest.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, session
 from flask_wtf import FlaskForm
 from wtforms import * # type: ignore
 from wtforms.validators import DataRequired
@@ -10,46 +10,56 @@ import requests
 
 googleMapsTest = Flask(__name__)
 googleMapsTest.config['SECRET_KEY'] = os.urandom(32)
-load_dotenv()
+load_dotenv() #Loads the .env file
+api_key = os.getenv('SETLIST_API_KEY')
+headers = {
+        "x-api-key": api_key,
+        "Accept": "application/json"
+}
+
+concertList = []
+
+@googleMapsTest.route('/storedata', methods=['POST']) #Appends the selected result to the list of concerts
+def storedata():
+    session['selectedResult'] = request.get_json()
+    concertList.append(session.get('selectedResult', [])[0])
+    return '', 200
 
 @googleMapsTest.route('/', methods=['GET', 'POST'])
 def testApi():
-    return render_template('googleMap.html')
+    mapCall = 'https://maps.googleapis.com/maps/api/js?key=' + os.getenv('GOOGLE_MAP_API') + '&callback=initMap'
+    return render_template('googleMap.html', mapCall=mapCall, concertList=concertList)
 
-@googleMapsTest.route('/search', methods=['GET', 'POST']) # type: ignore #Search page
+@googleMapsTest.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
-    artist = request.form.get('query')
-    date = request.form.get('date')
-    
+    artist = request.form.get('query') # Result from search bar
+    date = request.form.get('date') #Result from date
+
     results = []
-    api_key = os.getenv('SETLIST_API_KEY')
-    headers = {
-        "x-api-key": api_key,
-        "Accept": "application/json"
-    }
     if artist and date:
         date_object = datetime.strptime(date, '%Y-%m-%d')
-        date = date_object.strftime('%d-%m-%Y')
+        date = date_object.strftime('%d-%m-%Y') #Formats form date into query param format
         query_params = {"artistName": artist, 
                         "date": date}  # Use the user input here
         endpoint = "https://api.setlist.fm/rest/1.0/search/setlists"
         response = requests.get(endpoint, params=query_params, headers=headers).json()
-        if response.get("setlist"):
-            for item in response.get("setlist"):
+
+        if response.get("setlist"): #If the response is not None
+            for item in response.get("setlist"): # adds multiple setlist details to an item in a dictionary
                 artist_name = item.get("artist").get("name")
                 concert_date = item.get("eventDate")
                 city = item.get("venue").get("city").get("name")
                 venue = item.get("venue").get("name")
                 venue_coords = item.get("venue").get("city").get("coords")
                 lat = venue_coords.get("lat")
-                long = venue_coords.get("long")
+                lng = venue_coords.get("long")
                 results.append({'artist': artist_name, 
                                 'date': concert_date, 
                                 'venue': venue,
                                 'city': city,
                                 'lat': lat,
-                                'long': long})
+                                'lng': lng})
         else:
             results = None
     return render_template('search.html', form=form, results=results)
