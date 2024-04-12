91cff8d57f4272e113e3ce33d2e79088b797119f
Bruce Casillas <bruce.casillas562@gmail.com>
2024-04-12 03:50:47 -0400

    Added date option to get a specific concert, sends the results of the search query to be displayed in search.html

--- googleMapsTest.py
+++ googleMapsTest.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import * # type: ignore
 from wtforms.validators import DataRequired
+from datetime import datetime
 import os
 import requests
 
@@ -15,26 +16,44 @@ def testApi():
 @googleMapsTest.route('/search', methods=['GET', 'POST']) # type: ignore #Search page
 def search():
     form = SearchForm()
-    search_query = request.form.get('query')
+    artist = request.form.get('query')
+    date = request.form.get('date')
+    
     results = []
-
     api_key = "KEY"
     headers = {
         "x-api-key": api_key,
         "Accept": "application/json"
     }
-    if search_query:
-        query_params = {"artistName": search_query}  # Use the user input here
-        endpoint = "https://api.setlist.fm/rest/1.0/search/artists"
+    if artist and date:
+        date_object = datetime.strptime(date, '%Y-%m-%d')
+        date = date_object.strftime('%d-%m-%Y')
+        query_params = {"artistName": artist, 
+                        "date": date}  # Use the user input here
+        endpoint = "https://api.setlist.fm/rest/1.0/search/setlists"
         response = requests.get(endpoint, params=query_params, headers=headers).json()
-        iter = 1
-        for item in response.get("artist"):
-            artist_name = item.get("name")
-            results.append({'title': artist_name})
+        if response.get("setlist"):
+            for item in response.get("setlist"):
+                artist_name = item.get("artist").get("name")
+                concert_date = item.get("eventDate")
+                city = item.get("venue").get("city").get("name")
+                venue = item.get("venue").get("name")
+                venue_coords = item.get("venue").get("city").get("coords")
+                lat = venue_coords.get("lat")
+                long = venue_coords.get("long")
+                results.append({'artist': artist_name, 
+                                'date': concert_date, 
+                                'venue': venue,
+                                'city': city,
+                                'lat': lat,
+                                'long': long})
+        else:
+            results = None
     return render_template('search.html', form=form, results=results)
 
 class SearchForm(FlaskForm):
     ids = StringField("ID",validators=[DataRequired()])
+    date = DateField('Pick a Date', format='%Y-%m-%d')
     submit = SubmitField("Submit")
     csrf_token = HiddenField()
 
