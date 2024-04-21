0c7ba2576abb90b7618be5bf0c183ee7c3637dc2
Bruce Casillas <bruce.casillas562@gmail.com>
2024-04-21 17:30:01 -0400

    Updated app.py to force local host

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ class SearchForm(FlaskForm):
 
 if __name__ == "__main__":
     # to run me from the command line: <flask --app main run> or <python app.py>                          
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
